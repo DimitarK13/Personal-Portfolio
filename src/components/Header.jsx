6d9e195d3573394846fe7576495d0cc0b420a15e
Dimitar <dimikalapocev@gmail.com>
2024-02-12 20:11:01 +0100

    feat(nav): create and style

---
+++ src/components/Header.jsx
@@ -0,0 +1,70 @@
+import iconMenu from '../assets/icons/icon-menu.svg';
+import { useState } from 'react';
+
+export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isStatusOpen, setIsStatusOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const toggleStatus = () => {
+    setIsStatusOpen(!isStatusOpen);
+  };
+
+  return (
+    <nav className='nav'>
+      <div className='container nav__container'>
+        <button
+          className='nav__toggle'
+          aria-controls='primary-nav'
+          aria-expanded={isNavOpen}
+          onClick={toggleNav}>
+          <img src={iconMenu} alt='Menu toggle open' />
+        </button>
+
+        <ul className='nav__list' id='primary-nav' aria-expanded={isNavOpen}>
+          <li className='nav__list-item active'>
+            <a href='index.html'>01. Home</a>
+          </li>
+          <li className='nav__list-item'>
+            <a href='about.html'>02. About</a>
+          </li>
+          <li className='nav__list-item'>
+            <a href='work.html'>03. Work</a>
+          </li>
+          <li className='nav__list-item'>
+            <a href='contact.html'>04. Contact</a>
+          </li>
+        </ul>
+
+        <div className='nav__availability-status'>
+          <div
+            className='nav__availability-status--toggle'
+            aria-controls='availability-status-message'
+            aria-expanded={isStatusOpen}
+            onClick={toggleStatus}>
+            <p className='body-s'>Current Status:</p>
+            <p className='body-m color--available font-weight--medium'>
+              Available
+            </p>
+          </div>
+
+          <div
+            className='nav__availability-status--message'
+            id='availability-status-message'
+            aria-expanded={isStatusOpen}>
+            <p className='nav__availability-status__title'>
+              Project Availability Status
+            </p>
+
+            <p className='body-s color--available'>
+              Ready to begin immediately. Contact me now to get started!
+            </p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
