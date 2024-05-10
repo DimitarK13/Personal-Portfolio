830282334d508e57704054a18e3af6b17bd349cf
Dimitar <dimikalapocev@gmail.com>
2024-05-10 13:35:53 +0200

    feat(page: work): create and style

--- src/routes/work.jsx
+++ src/routes/work.jsx
@@ -1,7 +1,32 @@
 export default function Work() {
   return (
     <>
-      <h1>Work</h1>
+      <div className='container container__padding-block'>
+        <h1 className='projects-heading'>
+          Explore the fine selection of projects
+        </h1>
+
+        <div className='projects'>
+          <h2 className='projects__heading'>
+            <a href='#'>Neotek Systems</a>
+          </h2>
+          <h2 className='projects__heading'>
+            <a href='#'>YouthCall Platform</a>
+          </h2>
+          <h2 className='projects__heading'>
+            <a href='#'>Personal Portfolio</a>
+          </h2>
+          <h2 className='projects__heading'>
+            <a href='#'>Strax Designs</a>
+          </h2>
+          <h2 className='projects__heading'>
+            <a href='#'>Forkify</a>
+          </h2>
+          <h2 className='projects__heading'>
+            <a href='#'>Personal Web App</a>
+          </h2>
+        </div>
+      </div>
     </>
   );
 }
